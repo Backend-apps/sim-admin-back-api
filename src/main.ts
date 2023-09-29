5aa4c879bcbec04638e661988db52eee727786b7
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-09-29 11:47:58 +0500

    push

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-const port = process.env.HTTP_PORT || 8080;
+const port = process.env.HTTP_PORT || 5030;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
